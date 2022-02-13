c8fa55e2d7f213173e46c563accd531c80631ca1
Sukriti Srivastava <79142553+sukscode@users.noreply.github.com>
2022-02-13 17:16:16 +0530

    mongo in node.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-import { assert } from "console";
+
 import { MongoClient } from "mongodb";
+import assert from "assert";
 // Connection URl
 const url="mongodb://localhost:27017";
 // database name
-const dbName="myproject";
+const dbName="fruitsDB";
 //mongoclient
 const client = new MongoClient(url);
 
@@ -13,5 +14,35 @@ client.connect(function(err){
     console.log("connected successfully to server");
 
     const db=client.db(dbName);
+    //insertDocuments(db,function(){
+        findDocuments(db,function(){
     client.close();
+    });
 });
+
+const insertDocuments=function(db,callback){
+    const collection=db.collection("fruits");
+    collection.insertMany([
+        {name:"Apple",score:8,review:"great fruit"},
+        {name:"Orange",score:6,review:"Kinda Sour"},
+        {name:"Banana",score:9,review:"Great Stuff!!"}
+    ],function(err,result){
+        assert.equal(err,null);
+        //assert.equal(3,result.result.n);
+        //assert.equal(3,result.ops.length);
+        console.log("Inserted 3 documents ");
+        callback(result);
+
+    });
+};
+
+
+const findDocuments=function(db,callback){
+    const collection=db.collection("fruits");
+    collection.find({}).toArray(function(err,fruits){
+        assert.equal(err,null);
+        console.log("found the records");
+        console.log(fruits);
+        callback(fruits);
+    });
+};
